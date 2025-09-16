427b88e3081a1db281bee54bb0a63f7ca40292d3
fazelehalipoor <148051136+fazelehalipoor@users.noreply.github.com>
2025-09-16 22:09:42 +0330

    Added additional JS

--- src/script.js
+++ src/script.js
@@ -17,6 +17,8 @@ function updateCity(event) {
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
+  let currentLocationElement = document.querySelector("#current-location");
+  currentLocationElement.innerHTML = moment.tz.guess();
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
